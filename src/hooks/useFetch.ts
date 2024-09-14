28306699be6d4ae25c43d9dedf54db74e5784d70
gil6464 <gil6464@gmail.com>
2024-09-14 23:39:29 +0300

    Created use fetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+interface FetchState<T> {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const useFetch = <T>(url: string): FetchState<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`Error  ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error: any) {
+        setError(error.message || "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
