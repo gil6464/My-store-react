ba4e06b5a5d4f4e8f0dcf9c9922c1604c83fd29f
gil6464 <gil6464@gmail.com>
2024-09-15 20:44:29 +0300

    Updated project to use vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
+    },
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
+});
