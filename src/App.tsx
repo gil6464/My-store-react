30d449a9936aa641693e1f8275818e4a5475ca51
gil6464 <gil6464@gmail.com>
2024-09-18 09:04:06 +0300

    Updated header to an icon, and app to use cart drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { ProductProvider } from "./contexts/ProductContext";
 import { CartProvider } from "./contexts/CartContext";
-import ProductList from "./components/ProductList/productList";
+import { ProductProvider } from "@contexts/ProductContext";
 import Header from "./components/Header/Header";
 import Cart from "./components/Cart/Cart";
+import ProductList from "@components/ProductList/productList";
+import { Button } from "react-bootstrap";
+import { CartDrawerProvider } from "@contexts/CartDrawerContext";
+import CartDrawer from "@components/Cart/CartDrawer";
 
 const App: React.FC = () => {
+  const [showCart, setShowCart] = useState(false);
+
   return (
-    <ProductProvider>
-      <CartProvider>
+    <CartProvider>
+      <CartDrawerProvider>
         <Header />
-        <Cart />
-        <ProductList />
-      </CartProvider>
-    </ProductProvider>
+        <div className="container">
+          <ProductProvider>
+            <ProductList />
+          </ProductProvider>
+          <CartDrawer />
+        </div>
+      </CartDrawerProvider>
+    </CartProvider>
   );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./Header.scss";
+import { Navbar, Nav, Badge } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { useCart } from "@hooks/useCart";
+import { useCartDrawer } from "@hooks/useCartDrawer";
 
 const Header: React.FC = () => {
-  return <h1>Welcome to my store! :)</h1>;
+  const { cartItems } = useCart();
+  const { toggleDrawer } = useCartDrawer();
+
+  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">My Store</Navbar.Brand>
+      <Nav className="ml-auto">
+        <Nav.Link onClick={toggleDrawer}>
+          <FontAwesomeIcon icon={faShoppingCart} />
+          <Badge pill bg="danger" className="ml-2">
+            {totalItems}
+          </Badge>
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default Header;
